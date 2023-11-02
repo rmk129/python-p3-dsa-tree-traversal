1f265ec9388040378914c47274c1337306935173
Romil <romil.m.kothari@gmail.com>
2023-11-02 17:18:44 -0400

    tests completed

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    print(self.root['id'] == id)
+
+    if self.root['id'] == id:
+      return self.root
+    else:
+      for child in self.root['children']:
+          new_tree = Tree(child)
+          return new_tree.get_element_by_id(id)
+          
